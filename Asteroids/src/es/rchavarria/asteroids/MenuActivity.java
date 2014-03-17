0b29c24421a1fe57bf11ba7e013a682d9c1d4f41
rchavarria <txingo@gmail.com>
2014-03-17 09:30:24 +0100

    add a new view to the project

--- Asteroids/src/es/rchavarria/asteroids/MenuActivity.java
+++ Asteroids/src/es/rchavarria/asteroids/MenuActivity.java
@@ -3,6 +3,7 @@ package es.rchavarria.asteroids;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MenuActivity extends Activity {
 
@@ -12,12 +13,21 @@ public class MenuActivity extends Activity {
         setContentView(R.layout.activity_menu);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    	super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.configure:
+    		break;
+    	case R.id.about:
+    		break;
+    	}
+    	return true;
+    }
 }
