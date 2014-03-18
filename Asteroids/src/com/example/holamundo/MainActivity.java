c02ad01d8833fbd3eaa5a15accc7eedbeed7ffff
rchavarria <txingo@gmail.com>
2014-03-18 12:52:59 +0100

    show preferences in a Toast

--- Asteroids/src/com/example/holamundo/MainActivity.java
+++ Asteroids/src/com/example/holamundo/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.holamundo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -31,17 +34,27 @@ public class MainActivity extends Activity {
 		case R.id.config:
 			launchPreferences(null);
 			break;
+		case R.id.search:
+			showPreferences(null);
+			break;
 		}
 		return true;
 	}
-	
+
 	public void launchAbout(View view) {
 		Intent i = new Intent(this, AboutActivity.class);
 		startActivity(i);
 	}
-	
+
 	public void launchPreferences(View view) {
 		Intent i = new Intent(this, Preferences.class);
 		startActivity(i);
+	}
+
+	public void showPreferences(View v) {
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+		String s = "notificaciones: " + pref.getBoolean("notificaciones", true)
+				+ ", distancia mínima: " + pref.getString("distancia", "?");
+		Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
 	}
 }
