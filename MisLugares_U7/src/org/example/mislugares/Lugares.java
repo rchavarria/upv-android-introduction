0c4112fbd1eefac2c7362418b8da0d59fdeacf44
rchavarria <txingo@gmail.com>
2014-04-09 08:32:42 +0200

    class Lugares will get data from LugaresBD

--- MisLugares_U7/src/org/example/mislugares/Lugares.java
+++ MisLugares_U7/src/org/example/mislugares/Lugares.java
@@ -3,80 +3,97 @@ package org.example.mislugares;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Lugares {
 
-    public static final String TAG = "TagLugares";
+	public static final String TAG = "TagLugares";
 	public static List<Lugar> vectorLugares = ejemploLugares();
-	public static GeoPunto posicionActual = new GeoPunto(0,0);
+	public static GeoPunto posicionActual = new GeoPunto(0, 0);
 
-    public Lugares() {
-         vectorLugares = ejemploLugares();
-    }
+	private static LugaresBD lugaresBD;
 
-    public static Lugar elemento(int id){
-          return vectorLugares.get(id);
-    }
+	public static void indicializaBD(Context contexto) {
+		lugaresBD = new LugaresBD(contexto);
+	}
 
-    public static void anyade(Lugar lugar){
-          vectorLugares.add(lugar);
-    }
+	public static Cursor listado() {
+		SQLiteDatabase bd = lugaresBD.getReadableDatabase();
+		return bd.rawQuery("SELECT * FROM lugares", null);
+	}
 
-    public static int nuevo(){
-          Lugar lugar = new Lugar();
-          vectorLugares.add(lugar);
-          return vectorLugares.size()-1;
-    }
+	public Lugares() {
+		vectorLugares = ejemploLugares();
+	}
 
-    public static void borrar(int id){
-          vectorLugares.remove(id);
-    }
+	public static Lugar elemento(int id) {
+		return vectorLugares.get(id);
+	}
 
-    public static int size() {
-          return vectorLugares.size();
-    }
+	public static void anyade(Lugar lugar) {
+		vectorLugares.add(lugar);
+	}
 
-    public static ArrayList<Lugar> ejemploLugares() {
-          ArrayList<Lugar> lugares = new ArrayList<Lugar>();
-          lugares.add(new Lugar("Escuela Politécnica Superior de Gandía",
-           "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
-           TipoLugar.EDUCACION,962849300, "http://www.epsg.upv.es",
-           "Uno de los mejores lugares para formarse.", 3));
+	public static int nuevo() {
+		Lugar lugar = new Lugar();
+		vectorLugares.add(lugar);
+		return vectorLugares.size() - 1;
+	}
 
-          lugares.add(new Lugar("Al de siempre",
-           "P.Industrial Junto Molí Nou - 46722, Benifla (Valencia)",
-           -0.190642, 38.925857, TipoLugar.BAR, 636472405, "",
-           "No te pierdas el arroz en calabaza.", 3));
+	public static void borrar(int id) {
+		vectorLugares.remove(id);
+	}
 
-          lugares.add(new Lugar("androidcurso.com",
-           "ciberespacio", 0.0, 0.0, TipoLugar.EDUCACION, 
-           962849300, "http://androidcurso.com",
-           "Amplia tus conocimientos sobre Android.", 5));
+	public static int size() {
+		return vectorLugares.size();
+	}
 
-          lugares.add(new Lugar("Barranco del Infierno",
-           "Vía Verde del río Serpis. Villalonga (Valencia)", 
-           -0.295058, 38.867180, TipoLugar.NATURALEZA,
+	public static ArrayList<Lugar> ejemploLugares() {
+		ArrayList<Lugar> lugares = new ArrayList<Lugar>();
+		lugares.add(new Lugar("Escuela Politécnica Superior de Gandía",
+				"C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
+				TipoLugar.EDUCACION, 962849300, "http://www.epsg.upv.es",
+				"Uno de los mejores lugares para formarse.", 3));
 
-0, "http://sosegaos.blogspot.com.es/2009/02/lorcha-villalonga-via-verde-del-rio.html",
-           "Espectacular ruta para bici o andar", 4));
+		lugares.add(new Lugar("Al de siempre",
+				"P.Industrial Junto Molí Nou - 46722, Benifla (Valencia)",
+				-0.190642, 38.925857, TipoLugar.BAR, 636472405, "",
+				"No te pierdas el arroz en calabaza.", 3));
 
-          lugares.add(new Lugar("La Vital",
-           "Avda. de La Vital, 0 46701 Gandía (Valencia)", 
-           -0.1720092, 38.9705949, TipoLugar.COMPRAS,
-           962881070, "http://www.lavital.es/",
-           "El típico centro comercial", 2));
+		lugares.add(new Lugar("androidcurso.com", "ciberespacio", 0.0, 0.0,
+				TipoLugar.EDUCACION, 962849300, "http://androidcurso.com",
+				"Amplia tus conocimientos sobre Android.", 5));
 
-          return lugares;
+		lugares.add(new Lugar(
+				"Barranco del Infierno",
+				"Vía Verde del río Serpis. Villalonga (Valencia)",
+				-0.295058,
+				38.867180,
+				TipoLugar.NATURALEZA,
 
-    }
-    
-    public static List<String> nameList() {
-    	List<Lugar> places = ejemploLugares();
-    	List<String> names = new ArrayList<String>(places.size());
-    	
-    	for (Lugar place : places) {
+				0,
+				"http://sosegaos.blogspot.com.es/2009/02/lorcha-villalonga-via-verde-del-rio.html",
+				"Espectacular ruta para bici o andar", 4));
+
+		lugares.add(new Lugar("La Vital",
+				"Avda. de La Vital, 0 46701 Gandía (Valencia)", -0.1720092,
+				38.9705949, TipoLugar.COMPRAS, 962881070,
+				"http://www.lavital.es/", "El típico centro comercial", 2));
+
+		return lugares;
+
+	}
+
+	public static List<String> nameList() {
+		List<Lugar> places = ejemploLugares();
+		List<String> names = new ArrayList<String>(places.size());
+
+		for (Lugar place : places) {
 			names.add(place.getNombre());
 		}
-    	
-    	return names;
-    }
-  }
+
+		return names;
+	}
+}
